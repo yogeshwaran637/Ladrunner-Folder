bf2ffe64f632f92323c6088b6c1f3e7c5f654434
08-04-23 <Git integration>
2023-04-08 15:29:30 +0530

    Second time change

--- Action.c
+++ Action.c
@@ -1,4 +1,14 @@
-Action()
+
+	Action()
 {
-	return 0;
+		web_custom_request("web_custom_request",
+		"URL=www.google.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+		return 0;
 }
